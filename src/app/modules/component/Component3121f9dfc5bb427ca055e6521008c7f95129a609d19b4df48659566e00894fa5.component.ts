c1714770d1913a8c5a16b48702d8e77e2bf4102a
hustacdjm <hustacdjm@gmail.com>
2025-03-29 10:18:27 -0700

    phaser3 sample game

--- src/app/modules/component/Component3121f9dfc5bb427ca055e6521008c7f95129a609d19b4df48659566e00894fa5.component.ts
+++ src/app/modules/component/Component3121f9dfc5bb427ca055e6521008c7f95129a609d19b4df48659566e00894fa5.component.ts
@@ -6,6 +6,8 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { ElementRef, OnInit, OnDestroy } from '@angular/core';
+import Phaser from 'phaser';
 
 @Component({
     selector     : 'Component3121f9dfc5bb427ca055e6521008c7f95129a609d19b4df48659566e00894fa5',
@@ -17,13 +19,62 @@ import { MatIconModule } from '@angular/material/icon';
     ]
     
 })
-export class Component3121f9dfc5bb427ca055e6521008c7f95129a609d19b4df48659566e00894fa5
+export class Component3121f9dfc5bb427ca055e6521008c7f95129a609d19b4df48659566e00894fa5 implements OnInit, OnDestroy 
 {
 
     @Input() data:any;
+
+    private game!: Phaser.Game;
     /**
      * Constructor
      */
-     constructor(public sanitizer:DomSanitizer){}
+     constructor(public sanitizer:DomSanitizer,private elementRef: ElementRef){}
+
+     ngOnInit(): void {
+        this.initGame();
+      }
+    
+      ngOnDestroy(): void {
+        if (this.game) {
+          this.game.destroy(true);
+        }
+      }
+    
+      private initGame() {
+        const config: Phaser.Types.Core.GameConfig = {
+          type: Phaser.AUTO,
+          parent: 'game-container',
+          width: 800,
+          height: 600,
+          physics: {
+            default: 'arcade',
+            arcade: {
+              gravity: { x: 150, y: 300 },
+              debug: false
+            }
+          },
+          scene: {
+            preload: this.preload,
+            create: this.create,
+            update: this.update
+          }
+        };
+    
+        this.game = new Phaser.Game(config);
+      }
+    
+      private preload(this: Phaser.Scene) {
+        this.load.image('ball', 'https://phaser.io/content/tutorials/making-your-first-phaser-3-game/part7/assets/sprites/pangball.png');
+      }
+    
+      private create(this: Phaser.Scene) {
+        const ball = this.physics.add.sprite(400, 100, 'ball');
+        ball.setBounce(0.8);
+        ball.setCollideWorldBounds(true);
+      }
+    
+      private update(this: Phaser.Scene) {
+        // Game logic updates can go here.
+      }
 
 }
