145e64a3fa2188f38f62eda094adef185b1ae899
hustacdjm <hustacdjm@gmail.com>
2025-04-02 21:50:48 -0700

    add feature: learning dialog interface

---
+++ src/app/modules/clients/learning-component-dialog/learning-component.dialog.ts
@@ -0,0 +1,35 @@
+import {AfterViewInit, Component, Inject, Input, ViewChild, ViewEncapsulation} from '@angular/core';
+import {MatRadioGroup, MatRadioModule} from '@angular/material/radio';
+import {CommonModule} from "@angular/common";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { ElementRef, OnInit, OnDestroy } from '@angular/core';
+import Phaser from 'phaser';
+import {MatDialog, MatDialogModule, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+
+
+@Component({
+  selector: 'learning-component-dialog',
+  templateUrl: 'learning-component.dialog.html',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule, FormsModule, ReactiveFormsModule, CommonModule,],
+})
+export class LearningComponentDialog {
+
+
+  constructor(
+    public dialogRef: MatDialogRef<LearningComponentDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+  ) {
+    
+  }
+
+  submit(){
+    this.dialogRef.close({blood: 3, skills: 2});
+  }
+
+}
